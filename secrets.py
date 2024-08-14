ff0c572fa8efb079925aa38773fd4e3b10561d4b
Kaitlyn Than <kaitlynthan@gmail.com>
2024-08-14 14:59:07 -0700

    first commit

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'ATTh6Fxecl',
+    'password' : '?pesjsvr2tnv',
+    'timezone' : "America/Los_Angeles", # http://worldtimeapi.org/timezones
+    'github_token' : 'fawfj23rakjnfawiefa',
+    'hackaday_token' : 'h4xx0rs3kret',
+    }
